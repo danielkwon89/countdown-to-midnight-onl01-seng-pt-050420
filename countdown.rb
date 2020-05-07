42a56ac8511552e10ba392bdc1e17de9311a3260
Daniel Kwon <danielkwon89@gmail.com>
2020-05-07 21:39:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,12 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  until number == 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
